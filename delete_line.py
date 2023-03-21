1e79a04a8bc9bd57f1d39be06dc21718ba6b7e42
Notespeak <notespeak@nts.am>
2023-03-21 23:01:23 +0300

    Error handling

--- delete_line.py
+++ delete_line.py
@@ -1,13 +1,30 @@
+#!/usr/bin/env python
+
 import sys
 
 def delete_line(file_path, line_number):
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
+    try:
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        sys.exit(1)
+    except IOError as e:
+        print(f"Error: Could not read file '{file_path}': {e}")
+        sys.exit(1)
+
+    if line_number < 1 or line_number > len(lines):
+        print(f"Error: Invalid line number {line_number}.")
+        sys.exit(1)
 
     with open(file_path, 'w') as file:
-        for index, line in enumerate(lines):
-            if index != line_number - 1:
-                file.write(line)
+        try:
+            for index, line in enumerate(lines):
+                if index != line_number - 1:
+                    file.write(line)
+        except IOError as e:
+            print(f"Error: Could not write to file '{file_path}': {e}")
+            sys.exit(1)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -15,5 +32,10 @@ if __name__ == "__main__":
         sys.exit(1)
 
     file_path = sys.argv[1]
-    line_number = int(sys.argv[2])
+    try:
+        line_number = int(sys.argv[2])
+    except ValueError:
+        print("Error: Line number must be an integer.")
+        sys.exit(1)
+
     delete_line(file_path, line_number)
